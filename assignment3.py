e8cbbb3cf693c74b37dc78f5fc262f9fa4ad09bb
Megawott <61297991+Megawott@users.noreply.github.com>
2020-10-18 20:31:15 -0500

    Added comments to main program file

--- assignment3.py
+++ assignment3.py
@@ -1,14 +1,22 @@
+# User class
 class User():
+
+	# Create new user with username and password
     def __init__(self, id, pas):
         self.id = id
         self.pas = pas
 
+	# Login as user
     def login(self, id, pas):
         print (self.id)
         if self.id == id and self.pas == pas:
             print ("Login success!")
 
+# Item class
 class Item(object):
+
+	# Create new item with an ID number, a name, a price, a category, a description, and a quantity
+	# Of these attributes, only the price and quantity can be modified after initialization
     def __init__(self, unq_id, name, price, category, description, qty):
         self.unq_id = unq_id
         self.product_name = name
@@ -23,10 +31,15 @@ class Item(object):
     def updateQuantity(self, quantity):
         self.qty = quantity
 
+# Cart class
 class Cart(object):
+
+	# Create a cart for the user and initialize a dictionary to store items held in the cart
     def __init__(self):
         self.content = dict()
 
+	# Add a new item to the cart
+	# Can add in bulk, but cannot exceed current quantity for the selected item
     def addToCart(self, item):
         if item.unq_id not in self.content:
             self.content.update({item.unq_id: item})
@@ -43,21 +56,25 @@ class Cart(object):
             else:
                 v[k] = item[k]
 
+	# Calculate the total cost of all items in the cart by going through the list and getting the sum
     def getTotal(self):
         return sum([v.price * v.qty for _, v in self.content.items()])
 
+	# Return the total number of items in the cart
     def get_num_items(self):
         return sum([v.qty for _, v in self.content.items()])
 
+	# Remove an item from the cart
     def removeFromCart(self, key):
         self.content.pop(key)
 
-
+# User login
 if __name__ == '__main__':
     User = User("admin", "admin")
     User.login(input("Enter Login ID:"),
               input("Enter password: "))
 
+	# Product information
     item1 = Item(1, "Chair", 5.76, "household items", "A wooden chair", 15)
     item2 = Item(2, "Desk", 8.24, "household items", "A wooden desk", 10)
     item3 = Item(3, "Great Expectations", 3.52, "books", "Book by Charles Dickens", 53)
@@ -66,7 +83,8 @@ if __name__ == '__main__':
     item6 = Item(6, "Pikachu Plush", 6.35, "toys", "Plush of Pikachu", 24)
     item7 = Item(7, "RC car", 7.23, "small electronics", "remote controlled car", 9)
     item8 = Item(8, "LED lights", 12.15, "small electronics", "Strip of LED lights", 42)
-
+	
+	# Initialize objects/variables
     itemList = ["Chair", "Desk", "Great Expectations", "Fahrenheit 451", "Lego Set", "Pikachu Plush", "RC car", "LED lights"]
     cart = Cart()
     loggedin = True
@@ -75,6 +93,7 @@ if __name__ == '__main__':
         print("Please log in")
     else:
 
+		# Display UI
         print("""
     What would you like to do?
           1) View Inventory
@@ -87,6 +106,7 @@ if __name__ == '__main__':
 
         answer = input("> ")
 
+		# Perform action based on user response
         while (answer != "7"):
             if (answer == "1"):
                 print(item1.product_name, item1.price, item1.description, item1.category, item1.qty)
